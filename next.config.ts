792a6de090bafa7f097429d10a6e27067ccf3a1e
Mukhlish20 <imammukhlish2004@gmail.com>
2026-01-19 21:16:40 +0700

    remove static export config from api routes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-
-  trailingSlash: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
